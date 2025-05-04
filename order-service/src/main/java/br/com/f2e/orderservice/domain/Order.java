35b0821a25de3bb1369e86ed2d38f66519988702
franklin.barreto <franklin-barreto@hotmail.com>
2025-05-04 14:09:59 -0300

    test(order): all tests from OrderTest passes (4#)

--- order-service/src/main/java/br/com/f2e/orderservice/domain/Order.java
+++ order-service/src/main/java/br/com/f2e/orderservice/domain/Order.java
@@ -1,26 +1,33 @@
 package br.com.f2e.orderservice.domain;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
 public class Order {
+    private final OrderStatus orderStatus;
+    private List<OrderItem> items = new ArrayList<>();
+
     public Order(UUID customerId, String mail, ShippingAddress address, List<OrderItem> items) {
+        this.items = items;
+        orderStatus = OrderStatus.PENDING;
     }
 
     public BigDecimal getTotal() {
-        throw new UnsupportedOperationException();
+        return items.stream().map(OrderItem::getTotalPrice).reduce(BigDecimal.ZERO,BigDecimal::add);
     }
 
     public int getTotalItems() {
-        throw new UnsupportedOperationException();
+       return  items.stream().map(OrderItem::getQuantity).reduce(0,Integer::sum);
     }
 
     public OrderStatus getStatus() {
-        throw new UnsupportedOperationException();
+        return orderStatus;
     }
 
     public List<OrderItem> getItems() {
-        return null;
+        return Collections.unmodifiableList(items);
     }
 }
