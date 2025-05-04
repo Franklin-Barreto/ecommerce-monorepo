ed5a100cbd140f459fdc67f8e5f8c16d4c0c11a3
franklin.barreto <franklin-barreto@hotmail.com>
2025-05-04 12:08:09 -0300

    test(order): define initial failing tests for Order and OrderItem using TDD (4#)

---
+++ order-service/src/main/java/br/com/f2e/orderservice/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package br.com.f2e.orderservice.domain;
+
+public enum OrderStatus {
+    PENDING
+}
---
+++ order-service/src/main/java/br/com/f2e/orderservice/domain/ShippingAddress.java
@@ -0,0 +1,6 @@
+package br.com.f2e.orderservice.domain;
+
+public class ShippingAddress {
+    public ShippingAddress(String street, String city, String state, String number, String country) {
+    }
+}
