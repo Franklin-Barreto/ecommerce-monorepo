d9a318967a31b91c7b533b1770a831cc75f76333
franklin.barreto <franklin-barreto@hotmail.com>
2025-05-04 14:09:59 -0300

    test(order): all tests from OrderTest passes (4#)

--- order-service/src/main/java/br/com/f2e/orderservice/domain/OrderItem.java
+++ order-service/src/main/java/br/com/f2e/orderservice/domain/OrderItem.java
@@ -1,8 +1,26 @@
 package br.com.f2e.orderservice.domain;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public class OrderItem {
+    private final int quantity;
+    private BigDecimal price;
+
     public OrderItem(UUID productId, String productName, String price, int quantity) {
+        this.price = new BigDecimal(price);
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return price.multiply(new BigDecimal(quantity));
     }
 }
