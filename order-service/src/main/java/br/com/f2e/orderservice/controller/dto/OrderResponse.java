e45626a052fa156fe712117f9f312df793768b89
franklin.barreto <franklin-barreto@hotmail.com>
2025-05-22 17:49:31 -0300

    test(order-service): add tests for findById method

--- order-service/src/main/java/br/com/f2e/orderservice/controller/dto/OrderResponse.java
+++ order-service/src/main/java/br/com/f2e/orderservice/controller/dto/OrderResponse.java
@@ -3,11 +3,12 @@ package br.com.f2e.orderservice.controller.dto;
 import br.com.f2e.orderservice.domain.Order;
 
 import java.util.List;
+import java.util.UUID;
 
-public record OrderResponse(List<ItemResponse> items, int totalItems, double totalValue) {
+public record OrderResponse(UUID id, List<ItemResponse> items, int totalItems, double totalValue) {
 
     public static OrderResponse toDto(Order order) {
         var itemsDto = order.getItems().stream().map(ItemResponse::toDto).toList();
-        return new OrderResponse(itemsDto, order.getTotalItems(), order.getTotal().doubleValue());
+        return new OrderResponse(order.getId(),itemsDto, order.getTotalItems(), order.getTotal().doubleValue());
     }
 }
