ac50b2fc076aa2220c022096b3e9610487c03bf6
franklin.barreto <franklin-barreto@hotmail.com>
2025-05-23 06:48:53 -0300

    test(OrderController): add test for fetching an order by ID via GET /orders/{id}

--- order-service/src/test/java/br/com/f2e/orderservice/controller/OrderControllerTest.java
+++ order-service/src/test/java/br/com/f2e/orderservice/controller/OrderControllerTest.java
@@ -6,7 +6,6 @@ import br.com.f2e.orderservice.domain.Order;
 import br.com.f2e.orderservice.domain.ShippingAddress;
 import br.com.f2e.orderservice.repository.OrderRepository;
 import br.com.f2e.orderservice.service.OrderService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +20,13 @@ import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -39,6 +40,7 @@ class OrderControllerTest {
     private static final UUID CUSTOMER_ID = UUID.randomUUID();
     private static final String CUSTOMER_EMAIL = "customer-email@outlook.com";
     private static final UUID ORDER_ID = UUID.fromString("11111111-1111-1111-1111-111111111111");
+    private static final String URI = "/orders";
 
     @Autowired
     private MockMvc mockMvc;
@@ -52,13 +54,13 @@ class OrderControllerTest {
 
         var orderRequest = new OrderRequest(CUSTOMER_ID, CUSTOMER_EMAIL, getShippingAddress(), getItems());
         var items = getItems().stream().map(OrderItemRequest::toEntity).toList();
-        var order = new Order(CUSTOMER_ID,CUSTOMER_EMAIL,getShippingAddress(),items);
+        var order = new Order(CUSTOMER_ID, CUSTOMER_EMAIL, getShippingAddress(), items);
 
-        setOrderId(order,ORDER_ID);
+        setOrderId(order, ORDER_ID);
 
         when(orderRepository.save(any(Order.class))).thenReturn(order);
 
-        mockMvc.perform(post("/orders")
+        mockMvc.perform(post(URI)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(orderRequest))
                         .accept(MediaType.APPLICATION_JSON))
@@ -75,17 +77,33 @@ class OrderControllerTest {
 
         var orderRequest = new OrderRequest(CUSTOMER_ID, CUSTOMER_EMAIL, getShippingAddress(), Collections.emptyList());
         var items = getItems().stream().map(OrderItemRequest::toEntity).toList();
-        var order = new Order(CUSTOMER_ID,CUSTOMER_EMAIL,getShippingAddress(),items);
+        var order = new Order(CUSTOMER_ID, CUSTOMER_EMAIL, getShippingAddress(), items);
 
-        setOrderId(order,ORDER_ID);
+        setOrderId(order, ORDER_ID);
 
         when(orderRepository.save(any(Order.class))).thenReturn(order);
 
-        mockMvc.perform(post("/orders")
+        mockMvc.perform(post(URI)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(orderRequest))
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturnOrderDetailsGivenValidOrderId() throws Exception {
+
+        var items = getItems().stream().map(OrderItemRequest::toEntity).toList();
+        var order = new Order(CUSTOMER_ID, CUSTOMER_EMAIL, getShippingAddress(), items);
+        setOrderId(order, ORDER_ID);
+
+        when(orderRepository.findById(ORDER_ID)).thenReturn(Optional.of(order));
+        mockMvc.perform(get(URI + "/" + ORDER_ID)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(ORDER_ID.toString()))
+                .andExpect(jsonPath("$.totalItems").value(3))
+                .andExpect(jsonPath("$.totalValue").value(25.00));
     }
 
     private ShippingAddress getShippingAddress() {
