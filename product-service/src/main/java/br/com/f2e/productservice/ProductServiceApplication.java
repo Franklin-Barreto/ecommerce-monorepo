a245b53c937ef162a63479e62d7bc916ecc98e35
franklin.barreto <franklin-barreto@hotmail.com>
2025-05-25 18:29:04 -0300

    chore(product-service): populate database with sample data for development
    - Adjust initial_script_changelog.xml
    - Enable jpa auditing
    - Add data.sql

--- product-service/src/main/java/br/com/f2e/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/br/com/f2e/productservice/ProductServiceApplication.java
@@ -2,7 +2,9 @@ package br.com.f2e.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProductServiceApplication {
 
