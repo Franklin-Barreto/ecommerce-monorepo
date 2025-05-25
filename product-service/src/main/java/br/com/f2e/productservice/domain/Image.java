3970fff2d32332b5b1721e671a79dda15bf98552
franklin.barreto <franklin-barreto@hotmail.com>
2025-05-25 17:15:39 -0300

    feat(domain): define relationships between entities

--- product-service/src/main/java/br/com/f2e/productservice/domain/Image.java
+++ product-service/src/main/java/br/com/f2e/productservice/domain/Image.java
@@ -2,13 +2,21 @@ package br.com.f2e.productservice.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Image extends BaseEntity {
 
     @Column(length = 200)
     private String altText;
+
     private boolean isMain;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
 
     protected Image() {}
 
--- product-service/src/main/java/br/com/f2e/productservice/domain/Product.java
+++ product-service/src/main/java/br/com/f2e/productservice/domain/Product.java
@@ -1,21 +1,43 @@
 package br.com.f2e.productservice.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Product extends BaseEntity {
 
     @Column(length = 200)
     private String name;
+
     @Column(length = 300, nullable = false)
     private String description;
+
     @Column(precision = 10, scale = 2, nullable = false)
     private BigDecimal price;
+
     private int quantity;
     private boolean isActive = true;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "brand_id", nullable = false)
+    private Brand brand;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final Set<Image> images = new HashSet<>();
 
     @SuppressWarnings("unused")
     protected Product() {}
