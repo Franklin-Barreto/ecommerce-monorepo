dbcfae06d1f80fb8027b5da1d93643a0d16e709e
franklin.barreto <franklin-barreto@hotmail.com>
2025-05-25 16:05:11 -0300

    feat(domain): model core domain entities

---
+++ product-service/src/main/java/br/com/f2e/productservice/domain/Category.java
@@ -0,0 +1,22 @@
+package br.com.f2e.productservice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Category extends BaseEntity {
+
+    @Column(length = 100, unique = true, nullable = false)
+    private String name;
+
+    @Column(length = 300)
+    private String description;
+
+    @SuppressWarnings("unused")
+    public Category() {}
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
